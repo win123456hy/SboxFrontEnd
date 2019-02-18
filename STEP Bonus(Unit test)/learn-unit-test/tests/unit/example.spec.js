1ce33bc8aadfc80a5dc54ebd6e793d952e6aa171
Đạt Dev <datamity@gmail.com>
2019-02-18 14:45:16 +0700

    [COMPLETE].

--- STEP Bonus(Unit test)/learn-unit-test/tests/unit/example.spec.js
+++ STEP Bonus(Unit test)/learn-unit-test/tests/unit/example.spec.js
@@ -1,12 +1,44 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, mount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
+    const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+  it('renders contain <h3>Essential Links</h3>', () => {
+    const wrapper = mount(HelloWorld, {
+      propsData: { msg: 'dat' },
+    });
+    expect(wrapper.html()).toContain('<h3>Essential Links</h3>');
+  });
+  it('Prop Validate ', () => {
+    const wrapper = mount(HelloWorld, {
+      propsData: { msg: '123' },
+    });
+    expect(Number.isInteger(wrapper.props().msg)).toBe(false);
+  });
+  it('render mydata passed', () => {
+    const wrapper = mount(HelloWorld);
+    const { vm } = wrapper;
+    expect(vm.myData).toBe(5);
+  });
+  it('Change my data by method passed', () => {
+    const wrapper = mount(HelloWorld);
+    const { vm } = wrapper;
+    const mockValue = 5645;
+    wrapper.vm.changeMyData(mockValue);
+    expect(vm.myData).toBe(mockValue);
+  });
+  it('Render mydata to template passed', () => {
+    const wrapper = mount(HelloWorld);
+    const mockData = 123;
+    wrapper.setData({
+      myData: mockData,
+    });
+    expect(wrapper.html()).toContain("<p>My data: 123</p>");
+  });
+});
